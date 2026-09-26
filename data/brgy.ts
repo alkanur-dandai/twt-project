6eb83e095b5c244063d50fa6997054ed77bc4a2f
Alkanur Dandai <alkanurdandai.acad@gmail.com>
2026-09-26 12:50:22 +0800

    just updated

--- data/brgy.ts
+++ data/brgy.ts
@@ -1,7 +1,12 @@
 
 
 import type { FeatureCollection } from "geojson";
+
 import barangaysData from "./Bongao1.json";
+import panglimasugalaData from "./panglimasugala.json";
 
 export const barangays =
   barangaysData as unknown as FeatureCollection;
+
+export const panglimasugala = 
+  panglimasugalaData as unknown as FeatureCollection;
---
+++ lib/municipalityColors.ts
@@ -0,0 +1,62 @@
+export const municipalityColors: Record<
+  string,
+  {
+    fill: string;
+    line: string;
+  }
+> = {
+  Bongao: {
+    fill: "#2386e3",
+    line: "#eceef5",
+  },
+
+  "Panglima Sugala": {
+    fill: "#f50808",
+    line: "#ffffff",
+  },
+
+  Simunul: {
+    fill: "#22c55e",
+    line: "#15803d",
+  },
+
+  Sitangkai: {
+    fill: "#eab308",
+    line: "#a16207",
+  },
+
+  "South Ubian": {
+    fill: "#a855f7",
+    line: "#7e22ce",
+  },
+
+  Tandubas: {
+    fill: "#f97316",
+    line: "#c2410c",
+  },
+
+  "Turtle Islands": {
+    fill: "#06b6d4",
+    line: "#0e7490",
+  },
+
+  "Mapun": {
+    fill: "#ec4899",
+    line: "#be185d",
+  },
+
+  "Languyan": {
+    fill: "#14b8a6",
+    line: "#0f766e",
+  },
+
+  "Sapa-Sapa": {
+    fill: "#8b5cf6",
+    line: "#6d28d9",
+  },
+
+  "Sibutu": {
+    fill: "#84cc16",
+    line: "#4d7c0f",
+  },
+};
